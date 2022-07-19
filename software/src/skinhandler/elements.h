5396a3698feb94aa771c80b6a633de63b9d4b8bd
dettus <dettus@wasserkocher.deep.space>
2022-07-19 23:44:50 +0200

    started the lookup table for the locations of the elements.

--- software/src/skinhandler/elements.h
+++ software/src/skinhandler/elements.h
@@ -1,16 +1,36 @@
+/*
+
+Copyright 2022, dettus@dettus.net
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this 
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+   this list of conditions and the following disclaimer in the documentation 
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+*/
+
+
 #ifndef ELEMENTS_H
 #define	ELEMENTS_H
 // the information in this file was taken from the "Winamp 2.X skinnig tutorial" Version 1.5.0, copyright by Fyre
-typedef struct _tElementLocations
-{
-	char *filename;
-	int startx;
-	int starty;
-	int dimx;
-	int dimy;
-} tElementLocations;
-
-typedef enum eElementID=
+typedef enum 
 {
 // from avs.bmp
 	AVS_CLOSE_BUTTON_PRESSED=0,
@@ -451,6 +471,16 @@ typedef enum eElementID=
 	VOLUME_100,
 	VOLUME_SLIDER_PRESSED,
 	VOLUME_SLIDER_UNPRESSED
-};
+} eElementID;
+typedef struct _tElements
+{
+	char *filename;
+	int startx;
+	int starty;
+	int dimx;
+	int dimy;
+	eElementID id;
+} tElements;
+
 	
 #endif
