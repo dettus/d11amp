b9f613314ab5c28904cf8da45978100d98bd57c6
dettus <dettus@wasserkocher.deep.space>
2022-07-30 15:44:43 +0200

    file open dialog.

--- software/src/gui/mainwindow.c
+++ software/src/gui/mainwindow.c
@@ -313,8 +313,27 @@ static gboolean mainwindow_mousereleased(GtkWidget *widget,GdkEventButton* event
 		}
 		if (pressed==PRESSED_OPEN)
 		{
+     
+			GtkWidget *dialog;
+
+			dialog = gtk_file_chooser_dialog_new ("Open File",
+					GTK_WINDOW(pThis->mainWindow),
+					GTK_FILE_CHOOSER_ACTION_OPEN,
+					"_Cancel", GTK_RESPONSE_CANCEL,
+					"_Open", GTK_RESPONSE_ACCEPT,
+					NULL);
+
+			if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
+			{
+				char *filename;
+
+				filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
+				decode_fileopen(pThis->pHandleDecoderMain,filename);
+				g_free (filename);
+			}
+
+			gtk_widget_destroy (dialog);
 			printf("pressed open\n");
-			decode_fileopen(pThis->pHandleDecoderMain,"test.mp3");
 		}
 		if (pressed==PRESSED_PLAY)
 		{
