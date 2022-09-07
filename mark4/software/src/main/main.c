55e19b54a6736bfba7d182dfae8b67d3f13f5896
Thomas Dettbarn <dettus@dettus.net>
2022-09-07 20:03:51 +0200

    first command line options

--- mark4/software/src/main/main.c
+++ mark4/software/src/main/main.c
@@ -28,14 +28,61 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <pthread.h>
 #include "controller.h"
 #include "datastructures.h"
+#include "version.h"
+
 
 typedef struct _tArguments
 {
 	int argc;
 	char** argv;
 } tArguments;
-
-int commandline_parse(char* argument,int sort0parse1)
+void print_header()
+{
+	printf("*** d11amp %d.%d%d\n",VERSION_MAJOR,VERSION_MINOR,VERSION_REVISION);
+	printf("*** dettus@dettus.net\n");
+	printf("*** (C)opyright 2022\n");
+	printf("********************\n");
+	printf("\n");
+}
+void print_license()
+{
+	printf("\n");
+	printf("Copyright 2022, dettus@dettus.net\n");
+	printf("\n");
+	printf("Redistribution and use in source and binary forms, with or without modification,\n");
+	printf("are permitted provided that the following conditions are met:\n");
+	printf("\n");
+	printf("1. Redistributions of source code must retain the above copyright notice, this \n");
+	printf("   list of conditions and the following disclaimer.\n");
+	printf("\n");
+	printf("2. Redistributions in binary form must reproduce the above copyright notice, \n");
+	printf("   this list of conditions and the following disclaimer in the documentation \n");
+	printf("   and/or other materials provided with the distribution.\n");
+	printf("\n");
+	printf("THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND\n");
+	printf("ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED \n");
+	printf("WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE \n");
+	printf("DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE \n");
+	printf("FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL \n");
+	printf("DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR \n");
+	printf("SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER \n");
+	printf("CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, \n");
+	printf("OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE \n");
+	printf("OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n");
+}
+void print_help(char* argv0)
+{
+	printf("%s [OPTIONS]\n",argv0);
+	printf("Where [OPTIONS] are\n");
+	printf("--bsd     Prints the license\n");
+	printf("--help    shows this help\n");
+	printf("--version prints of %d.%d%d\n",VERSION_MAJOR,VERSION_MINOR,VERSION_REVISION);
+}
+void print_version()
+{
+	printf("%d.%d%d\n",VERSION_MAJOR,VERSION_MINOR,VERSION_REVISION);
+}
+int commandline_parse(char* argv0,char* argument,int sort0parse1)
 {
 	int l;
 	int retval;
@@ -44,11 +91,19 @@ int commandline_parse(char* argument,int sort0parse1)
 	l=strlen(argument);
 	if (l==5 && strncmp("--bsd",argument,l)==0)
 	{
-		retval=1;
+		print_license();
+		retval=2;
 	}
 	if (l==6 && strncmp("--help",argument,l)==0)
 	{
-		retval=1;
+		print_header();
+		print_help(argv0);
+		retval=2;
+	}
+	if (l==9 && strncmp("--version",argument,l)==0)
+	{
+		print_version();
+		retval=2;
 	}
 	return retval;
 }
@@ -98,11 +153,19 @@ int main(int argc,char** argv)
 
 	for (i=1;i<argc;i++)
 	{
-		if (commandline_parse(argv[i],0))
+		int retval;
+		retval=commandline_parse(argv[0],argv[i],0);
+		switch(retval)
 		{
-			arguments_d11amp.argv[arguments_d11amp.argc++]=argv[i];
-		} else {
-			arguments_gtk.argv[arguments_gtk.argc++]=argv[i];
+			case 2:
+				return 0;
+				break;
+			case 1:
+				arguments_d11amp.argv[arguments_d11amp.argc++]=argv[i];
+				break;
+			default:
+				arguments_gtk.argv[arguments_gtk.argc++]=argv[i];
+				break;
 		}	
 	}
 
