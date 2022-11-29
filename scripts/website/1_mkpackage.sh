a0bfe2f868bf98ad58c160feaf61d47c0fc045d4
Thomas Dettbarn <dettus@dettus.net>
2022-11-29 09:59:13 +0100

    prepared version 0.60

--- scripts/website/1_mkpackage.sh
+++ scripts/website/1_mkpackage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export VERSION=59
+export VERSION=0.60
 export SOFTWARE_ROOT=../../../../mark4/software
 
 
@@ -8,8 +8,8 @@ date
 mkdir package
 (
   cd package
-  mkdir d11amp_0.$VERSION
-  cd d11amp_0.$VERSION
+  mkdir d11amp_$VERSION
+  cd d11amp_$VERSION
 
   echo "creating directories"
   for I in `grep /$ ../../files.mft`
@@ -23,18 +23,18 @@ mkdir package
   done
 
   cd ../
-  tar cvfj ../d11amp_0.${VERSION}.tar.bz2 d11amp_0.${VERSION}/
+  tar cvfj ../d11amp_${VERSION}.tar.bz2 d11amp_${VERSION}/
 )
 
-ls -l d11amp_0.${VERSION}.tar.bz2
-ln -f d11amp_0.${VERSION}.tar.bz2 d11amp_latest.tar.bz2
+ls -l d11amp_${VERSION}.tar.bz2
+ln -f d11amp_${VERSION}.tar.bz2 d11amp_latest.tar.bz2
 
 (
   echo "checking package"
   mkdir tmp
   cd tmp
-  tar xvfj ../d11amp_0.${VERSION}.tar.bz2
-  cd d11amp_0.${VERSION}
+  tar xvfj ../d11amp_${VERSION}.tar.bz2
+  cd d11amp_${VERSION}
   make
   make SHA256_CMD=sha256sum check || make check
 )
@@ -47,10 +47,10 @@ rm -rf tmp/
 #echo "  cp old_releases.html ../templates/"
 #
 #
-md5sum d11amp_0.${VERSION}.tar.bz2 || md5 d11amp_0.${VERSION}.tar.bz2
+md5sum d11amp_${VERSION}.tar.bz2 || md5 d11amp_${VERSION}.tar.bz2
 md5sum d11amp_latest.tar.bz2 || md5 d11amp_latest.tar.bz2
 date
 
 echo "When you are satisfied, please run "
-echo " python3 2_mkhtml.py 0.${VERSION}"
-echo " python3 3_upload.py 0.${VERSION}"
+echo " python3 2_mkhtml.py ${VERSION}"
+echo " python3 3_upload.py ${VERSION}"
