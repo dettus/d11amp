7ea10e62da16cbd5d260a312ff5fde1b8f6ffd90
dettus <dettus@wasserkocher.deep.space>
2023-01-05 03:06:56 +0100

    this is release 0.60

--- scripts/website/screenshots/mkthumbs.sh
+++ scripts/website/screenshots/mkthumbs.sh
@@ -1,4 +1,5 @@
 convert d11amp001.png -scale 274x135 t_d11amp001.png
 convert d11amp058.png -scale 274x135 t_d11amp058.png
 convert d11amp059.png -scale 274x135 t_d11amp059.png
+convert d11amp060.png -scale 274x135 t_d11amp060.png
 
